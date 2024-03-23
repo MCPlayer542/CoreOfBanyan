b51167927fbd78a652e6a6dca1bae8e36bd0dc7b
hfos <xb82@outlook.com>
2024-03-23 19:41:37 +0800

    add some shit

--- Assets/Scripts/LandBehaviour.cs
+++ Assets/Scripts/LandBehaviour.cs
@@ -5,7 +5,8 @@ using System.Runtime.InteropServices;
 using System.Security;
 using UnityEngine;
 
-public enum Neighbor{
+public enum Neighbor
+{
     Left = 1,
     Right = 2,
     LUp = 4,
@@ -13,12 +14,13 @@ public enum Neighbor{
     LDown = 16,
     RDown = 32
 };
-public class NeighborPos{
-    static public Vector2Int Left=new(-1,-1), Right=new(1,1), RUp=new(1,0), LDown=new(-1,0), RDown=new(0,1), LUp=new(0,-1);
+public class NeighborPos
+{
+    static public Vector2Int Left = new(-1, -1), Right = new(1, 1), RUp = new(1, 0), LDown = new(-1, 0), RDown = new(0, 1), LUp = new(0, -1);
 }
 public class LandBehaviour : MonoBehaviour
 {
-    const float k3=0.5f;
+    const float k3 = 0.5f;
     public static GameServer s;
     public List<Color> colors;
     // Start is called before the first frame update
@@ -26,33 +28,35 @@ public class LandBehaviour : MonoBehaviour
     public float hp;
     public Neighbor neighbor;
     public bool nearPlayer, nearRoot;
-    public GameObject mPest=null;
-    public GameObject mFruit=null;
+    public GameObject mPest = null;
+    public GameObject mFruit = null;
 
     void Awake()
     {
         owner = -1;
         hp = 1;
-        colors = new(){Color.green, Color.red};
+        colors = new() { Color.green, Color.red };
         neighbor = 0;
     }
 
     // Update is called once per frame
-    readonly float Constant1 = 0.2f,Constant2 = 0.5f;
+    readonly float Constant1 = 0.2f, Constant2 = 0.5f;
     void Update()
     {
-        if(owner == -1)return;
-        if(nearRoot&&nearPlayer) s.players[owner].energy+=k3*Time.smoothDeltaTime;
-        if(nearRoot) hp += Constant1 * Time.smoothDeltaTime;
+        if (owner == -1) return;
+        if (nearRoot && nearPlayer) s.players[owner].energy += k3 * Time.smoothDeltaTime;
+        if (nearRoot) hp += Constant1 * Time.smoothDeltaTime;
         else hp -= Constant2 * Time.smoothDeltaTime;
-        if(mPest != null) hp -= Constant1 * Time.smoothDeltaTime;
-        
-        if(hp<=1){
-            Captured(-1,neighbor,1);
-            neighbor=0;
+        if (mPest != null) hp -= Constant1 * Time.smoothDeltaTime;
+
+        if (hp <= 1)
+        {
+            Captured(-1, neighbor, 1);
+            neighbor = 0;
         }
     }
-    public void Captured(int new_owner,Neighbor new_neighbor, float new_hp) {
+    public void Captured(int new_owner, Neighbor new_neighbor, float new_hp)
+    {
         var cur = s.PosToCell(transform.position);
         if (owner != -1) s.ChangeNeighborOfNeighbor(cur.x, cur.y, new_neighbor);
         owner = new_owner;
@@ -63,19 +67,29 @@ public class LandBehaviour : MonoBehaviour
         hp = new_hp;
         ChangeImg();
     }
-    public void ChangeImg() {
-        if(owner==-1) transform.GetChild(6).GetComponent<SpriteRenderer>().color = Color.white;
+    public void ChangeImg()
+    {
+        if (owner == -1) transform.GetChild(6).GetComponent<SpriteRenderer>().color = Color.white;
         else transform.GetChild(6).GetComponent<SpriteRenderer>().color = colors[owner];
-        for(int i=0;i<6;++i){
+        for (int i = 0; i < 6; ++i)
+        {
             SpriteRenderer s = transform.GetChild(i).GetComponent<SpriteRenderer>();
-            if(((int)neighbor&(1<<i))==0)
+            if (((int)neighbor & (1 << i)) == 0)
                 s.color = Color.white;
-            else {
-                if(owner == -1) s.color = Color.white;
+            else
+            {
+                if (owner == -1) s.color = Color.white;
                 else s.color = colors[owner];
             }
         }
     }
-    public float GetFruitsEnergy(){return mFruit.GetComponent<FruitBehavior>().getEnergy();}
-    public float GetPestsEnergy(){return 0;}
+    public float GetFruitsEnergy() { return mFruit.GetComponent<FruitBehavior>().getEnergy(); }
+    public float GetPestsEnergy() { return 0; }
+
+    public void EndGame()
+    {
+        if (mFruit != null) Destroy(mFruit);
+        if (mPest != null) Destroy(mPest);
+        Destroy(transform.gameObject);
+    }
 }
--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -13,11 +13,10 @@ public class PlayerBehaviour : MonoBehaviour
     public float speed;
     public float energy;
     public Vector2Int curpos;
-    GameServer s;
+    public static GameServer s;
     // Start is called before the first frame update
     void Start()
     {
-        s = Camera.main.GetComponent<GameServer>();
         transform.position = s.bornPos[pid];
         speed = 3.0f;
         curpos = s.PosToCell(s.bornPos[pid]);
@@ -39,17 +38,19 @@ public class PlayerBehaviour : MonoBehaviour
     void Update()
     {
         if (s.GameOverFlag) return;
-        if(Input.GetKeyDown(s.keySet[pid].Back)&&curpos!=s.PosToCell(s.bornPos[pid]))
+        if (Input.GetKeyDown(s.keySet[pid].Back) && curpos != s.PosToCell(s.bornPos[pid]))
         {
-            s.LBmap[curpos.x][curpos.y].Captured(-1,s.LBmap[curpos.x][curpos.y].neighbor,1);
-            s.LBmap[curpos.x][curpos.y].neighbor=0;
+            s.LBmap[curpos.x][curpos.y].Captured(-1, s.LBmap[curpos.x][curpos.y].neighbor, 1);
+            s.LBmap[curpos.x][curpos.y].neighbor = 0;
         }
-        if (s.LBmap[curpos.x][curpos.y].owner == -1){
+        if (s.LBmap[curpos.x][curpos.y].owner == -1)
+        {
             s.BackHome(pid);
             s.UpdateMap();
         }
         Vector3 p = transform.position;
-        if(s.ControlType==0){
+        if (s.ControlType == 0)
+        {
             int tx = 0, ty = 0;
             if (Input.GetKey(s.keySet[pid].Up)) ty++;
             if (Input.GetKey(s.keySet[pid].Down)) ty--;
@@ -70,7 +71,8 @@ public class PlayerBehaviour : MonoBehaviour
                 if (ty == -1) p += speed * Time.smoothDeltaTime * DirVector.Down;
             }
         }
-        else{
+        else
+        {
 
         }
 
@@ -181,14 +183,14 @@ public class PlayerBehaviour : MonoBehaviour
         if (curLand.owner != -1 && s.players[curLand.owner].curpos == cur && energy <= s.players[curLand.owner].energy + curLand.hp)
             return false;
         Neighbor tmp = curLand.neighbor;
-        curLand.neighbor=0;
+        curLand.neighbor = 0;
         if (!TryConnect(curLand, preLand, cur, pre))
         {
             curLand.neighbor = tmp;
             return false;
         }
         energy -= curLand.hp;
-        curLand.Captured(pid,tmp,5);
+        curLand.Captured(pid, tmp, 5);
         preLand.ChangeImg();
         return true;
     }
@@ -212,5 +214,9 @@ public class PlayerBehaviour : MonoBehaviour
             }
         }
         return true;
+    }
+    public void EndGame()
+    {
+        Destroy(transform.gameObject);
     }
 }
