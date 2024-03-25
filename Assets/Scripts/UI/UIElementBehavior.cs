c5aeb729d5314c3d882fb1100e44f096bd53dff9
hfos <xb82@outlook.com>
2024-03-25 17:40:48 +0800

    shit

---
+++ Assets/Scripts/UI/UIElementBehavior.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.ParticleSystemJobs;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class UIElementBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    static float Width = 600, Height = 400;
+
+    static float cursorFloat = 30;
+
+    public Vector3 P1, P2;
+    float sizeOfFont;
+    bool UIStatus = false;
+
+    public bool isBG = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var t1 = transform.GetChild(0).GetChild(0).GetComponent<TMP_Text>();
+        var t2 = transform.GetChild(0).GetChild(1).GetComponent<RawImage>();
+        if (!UIStatus)
+        {
+            sizeOfFont = t1.fontSize;
+            P1 = t1.rectTransform.localPosition;
+            P2 = t2.rectTransform.localPosition;
+            UIStatus = true;
+        }
+
+        Vector3 p1 = P1, p2 = P2;
+
+        //Debug.Log(p1);
+        if (isBG)
+        {
+            p1 = new(p1.x + cursorFloat * Input.mousePosition.x / Screen.width, p1.y + cursorFloat * Input.mousePosition.y / Screen.height, p1.z);
+            p2 = new(p2.x + cursorFloat * Input.mousePosition.x / Screen.width, p2.y + cursorFloat * Input.mousePosition.y / Screen.height, p2.z);
+        }
+
+        float scale = Math.Max(Screen.width / Width, Screen.height / Height);
+        t1.fontSize = sizeOfFont * scale;
+        t1.rectTransform.localPosition = new(p1.x * scale, p1.y * scale, p1.z);
+        t2.rectTransform.localPosition = new(p2.x * scale, p2.y * scale, p2.z);
+        t2.rectTransform.localScale = new(scale, scale, 0);
+    }
+}
