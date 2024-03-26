4606f6e511c218639aa3aa73849f38d0f000c098
hfos <xb82@outlook.com>
2024-03-27 02:08:05 +0800

    pull

--- Assets/Scripts/TutorialServer.cs
+++ Assets/Scripts/TutorialServer.cs
@@ -122,22 +122,22 @@ public class TutorialServer : GameServer
         {
             case 11:
                 text.SetText("按住A和D来进行左右移动，吃掉场地中间的苹果，注意每隔一段时间才能移动一次！");
-            break;
+                break;
             case 12:
                 text.SetText("你有没有注意到吃掉苹果时飘起的数字？核心上方的深蓝色数字代表你的创造力，吃苹果时会增加！");
-            break;
+                break;
             case 13:
-                text.SetText("现在同时按住D和W，进行斜向移动，吃掉场地角落的苹果；你也可以用WASD的其他组合来进行类似的斜向移动！");
-            break;
+                text.SetText("现在同时按住 D 和 W，进行斜向移动，吃掉场地角落的苹果；你也可以用 WASD 的其他组合来进行类似的斜向移动！");
+                break;
             case 14:
                 text.SetText("苹果在生成后一段时间会闪烁，不及时吃掉的话会消失！");
-            break;
+                break;
             case 15:
                 gm.NewTutorial();
                 break;
             case 21:
                 text.SetText("去到树枝末端消灭害虫，注意树枝是不能长成回路的！");
-            break;
+                break;
             case 22:
                 text.SetText("你有没有注意到被害虫侵袭的树枝上的数字减少了？树枝上的黑色数字代表坚固性，害虫会啃食你的枝干，减少到1后会断开，注意及时清理！");
                 break;
@@ -155,28 +155,28 @@ public class TutorialServer : GameServer
                 break;
             case 32:
                 text.SetText("你可以使用“落叶归根”快速回到根，使你的创造力恢复增长！");
-            break;
+                break;
             case 33:
                 text.SetText("非常棒！现在移动回去接上树枝，使树枝的坚固性恢复增长，避免树枝消亡！");
                 break;
             case 34:
                 text.SetText("现在树枝有点脆弱，可以按数字键2来使用“固若金汤”，消耗一定能量加固脚下的和与你直接相连的树枝！");
-            break;
+                break;
             case 35:
                 text.SetText("根非常重要，它是你的一切能量来源。和树根连通的树枝越多，你的创造力增长就越快，同时只有与根连通的地方才会结果或生虫！");
-            break;
+                break;
             case 36:
                 gm.NewTutorial();
                 break;
             case 41:
                 text.SetText("现在我们进入实战！看到中间的那个榕树核心了吗？积累一定创造力后，我们可以移动到它的树枝上来占领它！");
-            break;
+                break;
             case 42:
                 text.SetText("它被我们打回根了！现在我们去占领它的根，消灭它吧！");
-            break;
+                break;
             case 43:
                 text.SetText("如果场上只有你一棵榕树，你就获得了胜利；也要当心不要被别人消灭了！");
-            break;
+                break;
         }
     }
     bool TutorialFinished()
