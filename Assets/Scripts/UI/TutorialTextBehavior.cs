1f2824fd48a3fbc665defe22c2afda11e51b6d4d
hfos <xb82@outlook.com>
2024-03-27 00:15:27 +0800

    TutorialTextElement

---
+++ Assets/Scripts/UI/TutorialTextBehavior.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TutorialTextBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetText(string s)
+    {
+        transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TMP_Text>().text = s;
+    }
+    public string GetText()
+    {
+        return transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<TMP_Text>().text;
+    }
+
+    void End()
+    {
+        Destroy(transform.gameObject);
+    }
+}
